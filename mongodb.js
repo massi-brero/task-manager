4b0aba36ee85b0b5e29a8054fc5c113a773e6705
massi.brero@gmail.com <massi.brero@gmail.com>
2021-01-25 22:57:24 +0100

    refactoring  db connection and inserting

--- mongodb.js
+++ mongodb.js
@@ -1,35 +1,36 @@
 // CRUD
-const mongodb = require('mongodb')
-const mongoClient = mongodb.MongoClient
+const { MongoClient } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const usersCollectionName = 'users'
+const tasksCollectionName = 'tasks'
 
-mongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
-    if (err) {
-        console.log('Unable to connect to a database')
-        return
+const tasks = [
+    {
+        description: '10th Task',
+        completed: true
+    },
+    {
+        description: '11th Task',
+        completed: false
+    },
+    {
+        description: '12th Task',
+        completed: true
     }
+]
+
+try {
+    const client = new MongoClient(connectionUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    client.connect()
 
     const db = client.db(databaseName)
-    const usersCollectionName = 'users'
-    const tasksCollectionName = 'tasks'
-
-    const tasks = [
-        {
-            description: '4th Task',
-            completed: true
-        },
-        {
-            description: '5th Task',
-            completed: false
-        },
-        {
-            description: '6th Task',
-            completed: true
-        }
-    ]
 
     const insertTasks = async (tasks) => {
         try {
@@ -38,39 +39,11 @@ mongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: t
         } catch (err) {
             console.log(err.message || 'Could not insert tasks.')
         } finally {
-            //client.close()
+            await client.close()
         }
     }
 
     insertTasks(tasks)
-
-    // db.collection('users').insertOne({
-    //     name: 'Frieda',
-    //     age: '18'
-    // }, (err, result) => {
-    //     if (err) {
-    //         console.log('error when inserting user...')
-    //         return
-    //     }
-    //
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Massi',
-    //         age: '28'
-    //     },
-    //     {
-    //         name: 'Joe',
-    //         age: '19'
-    //     },
-    // ], (err, result) => {
-    //     if (err) {
-    //         console.log('error when inserting users...')
-    //         return
-    //     }
-    //
-    //     console.log(result.ops)
-    // })
-})
+} catch (err) {
+    console.log('Unable to connect to a database: ', err.message)
+}
