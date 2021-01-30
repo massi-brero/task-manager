1c8d9ba0c80831040a5c1352fc5587d6bb23851f
massi.brero@gmail.com <massi.brero@gmail.com>
2021-01-30 15:42:45 +0100

    find

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,13 @@
 // CRUD
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-const { MongoClient, ObjectID } = require('mongodb')
+const {MongoClient, ObjectID} = require('mongodb')
 
 const usersCollectionName = 'users'
 const tasksCollectionName = 'tasks'
 
 const id = new ObjectID()
+let db = null;
 
 const tasks = [
     {
@@ -25,9 +26,16 @@ const tasks = [
 
 const users = [
     {
-        _id: id,
-        name: 'Massi',
-        age: 50
+        name: 'Joe',
+        age: 22
+    },
+    {
+        name: 'Frieda',
+        age: 34
+    },
+    {
+        name: 'Sören',
+        age: 43
     }
 ]
 
@@ -39,12 +47,13 @@ const run = async () => {
     })
 
     try {
-        await client.connect()
-        const db = client.db(databaseName)
+        const res = await client.connect()
+        db = client.db(databaseName)
 
-        //await insertData(usersCollectionName, users, db)
-        //await insertTasks(tasks)
-        await findUser(usersCollectionName, {name: 'Massi'}, db)
+        // await insertData(tasksCollectionName, tasks, db)
+        // await find(tasksCollectionName, {_id: new ObjectID('6015648a9502a1827f1201cd')}, db)
+        // await find(tasksCollectionName, {completed: true}, db)
+        await update(usersCollectionName, {_id: new ObjectID('60155ea537665880efc00eba')}, {name: 'Jupiter'})
     } catch (err) {
         console.log('Unable to connect to a database: ', err.message)
     } finally {
@@ -52,24 +61,42 @@ const run = async () => {
     }
 }
 
-const findUser = async (collection, search, db) => {
+const find = async (collection, search, db) => {
     try {
-        const result = await db.collection(collection).findOne(search)
-        console.log(result)
+        const result = await db.collection(collection).find(search).toArray()
+        console.log(
+            result
+        )
     } catch (err) {
         console.log(`Unable to fetch: ${err.message}`)
     }
 }
 
-const insertData = async (collection, data, db) => {
+const insert = async (collection, data, db) => {
     try {
         const result = await db.collection(collection).insertMany(data)
         console.log(result.ops)
     } catch (err) {
-        console.log(err.message || 'Could not insert data.')
+        console.log(err.message || 'Could not insert document.')
+    }
+}
+
+const update = async (collection, search, set) => {
+    try {
+        const result = await db.collection(collection).updateMany(
+            search,
+            {
+                $set: set
+            }
+        )
+        console.log(
+            result
+        )
+    } catch (err) {
+        console.log(err.message || 'Could not update documents.')
     }
 }
 
 run().then(() => {
-    console.log('app started...')
+    console.log('app ran...')
 })
