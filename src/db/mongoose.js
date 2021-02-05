b85d8c5700db9bc746e2f2b026691363f7733e98
massi.brero@gmail.com <massi.brero@gmail.com>
2021-02-05 23:08:40 +0100

    validation 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,39 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 const dbUrl = 'mongodb://localhost:27017/task-manager-api'
 
 const run = async () => {
 
     const User = mongoose.model('User', {
         name: {
-            type: String
+            type: String,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) throw new Error('Invalid email address.')
+            }
         },
         age: {
-            type: Number
+            type: Number,
+            default: 0,
+            trim: true,
+            validate(value) {
+                if (value < 0) throw new Error('Age must be a positive number.')
+            }
         }
+    })
+
+
+    const me = new User({
+        name: '   Jeff  ',
+        email: 'Jeff@fritze.de',
+        age: 23
     })
 
     const Task = mongoose.model('Task', {
@@ -19,6 +43,11 @@ const run = async () => {
         completed: {
             type: Boolean
         }
+    })
+
+    const task1 = new Task({
+        description: 'Task 1',
+        completed: false
     })
 
     try {
@@ -31,19 +60,9 @@ const run = async () => {
         console.log(e.message)
     }
 
-    const me = new User({
-        name: 'Massi',
-        age: 50
-    })
-
-    const task1 = new Task({
-        description: 'Task 1',
-        completed: false
-    })
-
     try {
-        //const result = await me.save()
-        const result = await task1.save()
+        const result = await me.save()
+        //const result = await task1.save()
         console.log(result)
     } catch (e) {
         console.log(e.message)
@@ -51,5 +70,5 @@ const run = async () => {
 }
 
 run().then(() => {
-    console.log('app ran...')
+    console.log('app started...')
 })
