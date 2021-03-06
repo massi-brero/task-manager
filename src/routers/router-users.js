8fb838d8cdb0faa466a58aea355917242f7a28aa
massi.brero@gmail.com <massi.brero@gmail.com>
2021-03-06 21:18:46 +0100

    jwt 2 challenge

--- src/routers/router-users.js
+++ src/routers/router-users.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const User = require('../models/user')
+const dbUtils = require('../db/db.utils')
 require('mongoose')
 
 
 const userRouter = new express.Router()
 module.exports = userRouter
+
+apiRouter.post('/api/users', async (req, res) => {
+
+        try {
+
+            const userExists = !!!User.findOne(req.body.email)
+
+            if (userExists) {
+                throw new Error('A user with that email already exists.')
+            }
+
+            const user = await dbUtils.getResource(req).save();
+
+            res
+                .status(201)
+                .send({
+                    user,
+                    token: user.generateAuthToken()
+                })
+        } catch (e) {
+            res
+                .status(400)
+                .send(e.message)
+        }
+
+})
 
 userRouter.post('/api/users/login', async (req, res) => {
     try {
