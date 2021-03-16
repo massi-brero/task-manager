dd109e57fa6385faa092efd87514bed6d54c7f45
massi.brero@gmail.com <massi.brero@gmail.com>
2021-03-16 21:33:59 +0100

    returning manipulated user object

--- src/routers/router-users.js
+++ src/routers/router-users.js
@@ -7,69 +7,61 @@ const userRouter = new express.Router()
 module.exports = userRouter
 
 userRouter.post('/api/users', async (req, res) => {
+  try {
+    const userExists = !!!User.findOne(req.body.email)
 
-        try {
+    if (userExists) {
+      throw new Error('A user with that email already exists.')
+    }
 
-            const userExists = !!!User.findOne(req.body.email)
+    const user = await new User(req.body).save()
 
-            if (userExists) {
-                throw new Error('A user with that email already exists.')
-            }
-
-            const user = await (new User(req.body)).save();
-
-            res
-                .status(201)
-                .send({
-                    user,
-                    token: await user.generateAuthToken()
-                })
-
-        } catch (e) {
-            res
-                .status(400)
-                .send(e.message)
-        }
-
+    res.status(201).send({
+      user,
+      token: await user.generateAuthToken(),
+    })
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
 })
 
 userRouter.post('/api/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({user, token})
-    } catch (err) {
-        console.log(err)
-        const status = err.message.includes(401) ? 401 : 400
-        res.status(status).send()
-    }
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (err) {
+    console.log(err)
+    const status = err.message.includes(401) ? 401 : 400
+    res.status(status).send()
+  }
 })
 
 userRouter.post('/api/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
 
-        await req.user.save()
+    await req.user.save()
 
-        res.send()
-    } catch (err) {
-        res.status(500).send()
-    }
+    res.send()
+  } catch (err) {
+    res.status(500).send()
+  }
 })
 
 userRouter.post('/api/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
+  try {
+    req.user.tokens = []
+    await req.user.save()
 
-        res.send()
-    } catch (err) {
-        res.status(500).send()
-    }
+    res.send()
+  } catch (err) {
+    res.status(500).send()
+  }
 })
 
 userRouter.get('/api/users/me', auth, async (req, res) => {
-    res.send(req.user)
+  res.send(req.user)
 })
